4c1abb83a0fa747e21c52011a54bcc00f09aa61d
muheenudeen <muheenudeen313@gmail.com>
2024-12-04 22:19:24 +0530

    Time: 0 ms (100%), Space: 49.2 MB (30.66%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.js
@@ -0,0 +1,33 @@
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function (digits) {
+    if (digits.length === 0) return [];
+
+    const keyboard = {
+        "2": "abc",
+        "3": "def",
+        "4": "ghi",
+        "5": "jkl",
+        "6": "mno",
+        "7": "pqrs",
+        "8": "tuv",
+        "9": "wxyz"
+    };
+
+    const output = [];
+    const backtrack = (current, index) => {
+        if (index === digits.length) {
+            output.push(current);
+            return;
+        }
+        const letters = keyboard[digits[index]];
+        for (let letter of letters) {
+            backtrack(current + letter, index + 1);
+        }
+    };
+
+    backtrack('', 0);
+    return output;
+};
