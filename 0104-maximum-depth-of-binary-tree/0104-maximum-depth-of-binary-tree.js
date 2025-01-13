319040159a4240be9047b63252452ec4686bc5ee
muheenudeen <muheenudeen313@gmail.com>
2025-01-13 17:26:38 +0530

    Time: 2 ms (20.84%), Space: 52.2 MB (50.68%) - LeetHub

---
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.js
@@ -0,0 +1,19 @@
+var maxDepth = function(root) {
+  if(!root) return 0;
+  
+  let maxDepthVal = currentDepthVal = 0;
+
+  const countLevel = (node) => {
+    if(!node) return maxDepthVal = Math.max(maxDepthVal, currentDepthVal);
+
+    ++currentDepthVal
+    countLevel(node.left)
+    countLevel(node.right)
+    
+    --currentDepthVal;
+  };
+
+  countLevel(root);
+
+  return maxDepthVal;
+};
