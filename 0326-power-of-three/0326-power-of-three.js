9b7ce7fb44aee7ebc2bc965c80e01149b5294114
muheenudeen <muheenudeen313@gmail.com>
2025-01-20 21:55:33 +0530

    Time: 5 ms (96.73%), Space: 59.7 MB (13.89%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    while (n > 1) {
+        if (n % 3 === 0) n /= 3;
+        else break;
+    }
+    return n === 1;
+};
