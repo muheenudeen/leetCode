960055f1b2b21a97366896eade712a3e9f2cf533
muheenudeen <muheenudeen313@gmail.com>
2025-01-26 21:10:50 +0530

    Time: 53 ms (17.65%), Space: 65.1 MB (86.27%) - LeetHub

---
+++ 0565-array-nesting/0565-array-nesting.js
@@ -0,0 +1,18 @@
+const arrayNesting = (nums) => {
+    let result = 0;
+    const obj = {};
+    for (let i = 0; i < nums.length; i++) {
+        if (obj[nums[i]] !== undefined) {continue;}
+        obj[nums[i]] = 1;
+        let curr = nums[nums[i]];
+        while(nums[i] !== curr) {
+            obj[nums[i]]++;
+            obj[curr] = true;
+            curr = nums[curr];
+        }
+        result = result < obj[nums[i]] ? obj[nums[i]] : result;
+    }
+    return result;
+};
+
+
