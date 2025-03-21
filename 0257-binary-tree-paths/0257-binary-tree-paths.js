1674bb0be28eab05127f22f4210607a2e0093045
muheenudeen <muheenudeen313@gmail.com>
2025-03-21 17:57:32 +0530

    Time: 1 ms (23.59%), Space: 54.9 MB (36.54%) - LeetHub

---
+++ 0257-binary-tree-paths/0257-binary-tree-paths.js
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {string[]}
+ */
+var binaryTreePaths = function(root) {
+    let paths = [];
+
+    function preOrder(node,currPath){
+        if(node){
+            currPath.push(node.val);
+            if(node.left==null && node.right==null){
+                paths.push([...currPath]);
+            }
+            preOrder(node.left,[...currPath]);
+            preOrder(node.right,[...currPath]);
+        }
+    }
+    preOrder(root,[]);
+    let ans = paths.map(path => path.join("->"))
+    return ans;
+};
