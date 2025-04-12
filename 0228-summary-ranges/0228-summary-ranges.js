8df80d46914bf6cc40ac73e0898f0c12c3d03fbb
muheenudeen <muheenudeen313@gmail.com>
2025-04-12 09:41:15 +0530

    Time: 0 ms (100%), Space: 53.8 MB (23.57%) - LeetHub

---
+++ 0228-summary-ranges/0228-summary-ranges.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @return {string[]}
+ */
+var summaryRanges = function(nums) {
+    
+    let start=0
+    const result =[]
+
+    for(let i=0;i<nums.length; i++) {
+        if(i===nums.length-1 || nums[i] +1 !==nums[i+1]){
+            if(start === i) {
+                result.push(`${nums[i]}`)
+            }else{
+                result.push(`${nums[start]}->${nums[i]}`);
+            }
+            start=i+1
+        }
+    }
+    return result
+};
