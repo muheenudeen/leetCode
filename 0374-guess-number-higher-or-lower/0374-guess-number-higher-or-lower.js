e445ca72a91f2f714c435e6778efb23de2fbf3c6
muheenudeen <muheenudeen313@gmail.com>
2025-04-14 22:54:33 +0530

    Time: 42 ms (59.09%), Space: 54.7 MB (6.1%) - LeetHub

--- 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
+++ 0374-guess-number-higher-or-lower/0374-guess-number-higher-or-lower.js
@@ -1,5 +1,4 @@
 /** 
- * Forward declaration of guess API.
  * @param {number} num   your guess
  * @return 	     -1 if num is higher than the picked number
  *			      1 if num is lower than the picked number
