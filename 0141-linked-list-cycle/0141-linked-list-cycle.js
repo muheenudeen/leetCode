62ae521bb70d2f314231dd4d32e39e7b448d4db7
muheenudeen <muheenudeen313@gmail.com>
2025-04-26 22:29:50 +0530

    Time: 44 ms (91.96%), Space: 59.3 MB (47.96%) - LeetHub

---
+++ 0141-linked-list-cycle/0141-linked-list-cycle.js
@@ -0,0 +1,14 @@
+var hasCycle = function(head) {
+    if (!head || !head.next) return false;
+
+    let slow = head;
+    let fast = head.next;
+
+    while (slow !== fast) {
+        if (!fast || !fast.next) return false;
+        slow = slow.next;
+        fast = fast.next.next;
+    }
+
+    return true;
+};
