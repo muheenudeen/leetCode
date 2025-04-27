512d521826369628c2e1e1a701882047033c176c
muheenudeen <muheenudeen313@gmail.com>
2025-04-27 21:01:36 +0530

    Time: 6 ms (94.14%), Space: 62.1 MB (47.85%) - LeetHub

---
+++ 0599-minimum-index-sum-of-two-lists/0599-minimum-index-sum-of-two-lists.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string[]} list1
+ * @param {string[]} list2
+ * @return {string[]}
+ */
+var findRestaurant = function(list1, list2) {
+    
+    let out= []
+    let obj = new Map()
+    list2.forEach((v, i)=> obj.set(v,i))
+    let i=0, min = Infinity
+    while(i<list1.length) {
+        if(obj.has(list1[i])) {
+            let sum = i +obj.get(list1[i])
+            if(min===sum) {
+                out.push(list1[i])
+            }
+            else if(sum < min) {
+                out = [list1[i]]
+                min = sum
+            }
+        }
+        i++
+    }
+    return out
+
+};
