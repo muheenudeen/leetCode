b8d941508c0150f643448fcacf04750f4ce7c7de
muheenudeen <muheenudeen313@gmail.com>
2025-05-01 19:54:50 +0530

    Time: 0 ms (100%), Space: 54.9 MB (80.44%) - LeetHub

--- 0136-single-number/0136-single-number.js
+++ 0136-single-number/0136-single-number.js
@@ -5,10 +5,12 @@
  */
 var singleNumber = function(nums) {
     let result = 0;
+
     for (let i = 0; i < nums.length; i++) {
         result ^= nums[i];
+        
     }
 
     return result;
-    
+
 };
