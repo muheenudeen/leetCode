1ebc42f704ce42ddbe82d35bd6e34ae3caef9e31
muheenudeen <muheenudeen313@gmail.com>
2025-05-14 23:46:42 +0530

    Time: 3 ms (55.35%), Space: 63.8 MB (7.24%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.js
+++ 0011-container-with-most-water/0011-container-with-most-water.js
@@ -7,6 +7,8 @@ var maxArea = function(height) {
     let ans = 0;
     let i = 0;
     let j = height.length - 1;
+
+    
     while (i < j) {
         ans = Math.max(ans, Math.min(height[i], height[j]) * (j - i));
         if (height[i] < height[j]) i++;  
