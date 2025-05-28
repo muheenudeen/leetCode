b3d52ae4eacae9e942c465170295a9cc9bcdf1e1
muheenudeen <muheenudeen313@gmail.com>
2025-05-28 22:32:30 +0530

    Time: 1 ms (89.63%), Space: 64.1 MB (22.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,13 +3,10 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-
     let sum=nums[0], max=nums[0]
-
     for(let i=1;i<nums.length;i++){
         sum= Math.max(nums[i], sum+nums[i])
         max=Math.max(max, sum)
     }
     return max
-
 };
